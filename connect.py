c447b2bab40a6cd2601908ef5541547be0742c8d
PatrickZou <Patrick.Zou@lincolnuni.ac.nz>
2023-12-01 01:50:06 +1300

    bug fix

--- connect.py
+++ connect.py
@@ -1,7 +1,7 @@
 # These details are available on the first MySQL Workbench screen
 # Usually called 'Local Instance'
 dbuser = "root" # Your MySQL username - likely 'root'
-dbpass = "Patrickzou123" # ---- PUT YOUR PASSWORD HERE ----
+dbpass = "Patrickzou123" # --- PUT YOUR PASSWORD HERE ---
 dbhost = "localhost" 
 dbport = "3306"
 dbname = "motorkhana"
